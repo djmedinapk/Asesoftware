5d25882de194b308afd0a9202cdd37f9196e84c8
djmedinapk <djmedinapk@gmail.com>
2021-06-03 16:29:56 -0500

    Add project files.

---
+++ Logic/Logic/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Logic
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> getUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<User>> getUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
